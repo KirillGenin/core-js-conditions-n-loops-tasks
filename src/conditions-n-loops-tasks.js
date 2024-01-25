37995a0262314256610562879580f2b28b603ea1
KirillGenin <332211op@gmail.com>
2024-01-25 15:55:20 +0300

    feat: implement getMaxNumber function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,14 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(...rest) {
+  let maxNumber = 0;
+
+  for (let i = 0; i < rest.length; i += 1) {
+    maxNumber = rest[i] > maxNumber ? rest[i] : maxNumber;
+  }
+
+  return maxNumber;
 }
 
 /**
